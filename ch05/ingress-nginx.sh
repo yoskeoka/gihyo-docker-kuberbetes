311d32783d90b5ab6f9e6bca4b5ee14f9d3bd1f9
gcoka <y.okamoto@gemcook.com>
2018-09-30 16:39:10 +0900

    [add] ch05

---
+++ ch05/ingress-nginx.sh
@@ -0,0 +1,2 @@
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.16.2/deploy/mandatory.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.16.2/deploy/provider/cloud-generic.yaml
